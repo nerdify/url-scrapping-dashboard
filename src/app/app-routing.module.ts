820024ce83bf52685bd32dfbfb815c2c8ec18df9
aguilarkevin <kevinaguilar0813@gmail.com>
2023-10-12 15:54:56 -0600

    registered some components and login route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core'
 import {RouterModule, Routes} from '@angular/router'
 import {DashboardLayoutComponent} from './dashboard-layout/dashboard-layout.component'
+import {LoginComponent} from './login/login.component'
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
     path: 'dashboard',
     component: DashboardLayoutComponent,
     children: [],
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
   },
 ]
 
