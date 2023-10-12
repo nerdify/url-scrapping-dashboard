cbefc8f58033e60feb9dc4e69ceab59591e9422f
aguilarkevin <kevinaguilar0813@gmail.com>
2023-10-12 12:29:43 -0600

    add some routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core'
+import {RouterModule, Routes} from '@angular/router'
+import {DashboardLayoutComponent} from './dashboard-layout/dashboard-layout.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    component: DashboardLayoutComponent,
+    children: [],
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
