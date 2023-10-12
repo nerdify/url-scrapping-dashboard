c7e8f89190bcf3e2a01680681db10f575941eb42
aguilarkevin <kevinaguilar0813@gmail.com>
2023-10-12 12:27:16 -0600

    registered some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module'
+import {AppComponent} from './app.component'
+import {DashboardLayoutComponent} from './dashboard-layout/dashboard-layout.component';
+import { PageComponent } from './page/page.component'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DashboardLayoutComponent, PageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
