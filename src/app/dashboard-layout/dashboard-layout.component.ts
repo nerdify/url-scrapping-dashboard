df9e3acb3eefbac58b8e5dbe5ee0c43efe9223a8
aguilarkevin <kevinaguilar0813@gmail.com>
2023-10-12 12:26:12 -0600

    create dashboard layout component

---
+++ src/app/dashboard-layout/dashboard-layout.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core'
+
+@Component({
+  selector: 'app-dashboard-layout',
+  templateUrl: './dashboard-layout.component.html',
+  styleUrls: ['./dashboard-layout.component.css'],
+})
+export class DashboardLayoutComponent {
+  isList = 0
+  isMenu = false
+  isSearch = false
+}
