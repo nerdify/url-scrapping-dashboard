998705b91e0ffa082a76557c99a8a8406377b978
aguilarkevin <kevinaguilar0813@gmail.com>
2023-10-13 08:14:12 -0600

    fixes

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,15 +1,51 @@
-import {Component, Input} from '@angular/core'
+import {Component, Input, forwardRef} from '@angular/core'
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms'
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor {
+  value: unknown = ''
+  touched = false
+  isDisabled = false
+
   @Input() label = ''
   @Input() placeholder = ''
   @Input() helperText = ''
   @Input() leftElement = ''
   @Input() type = 'text'
   @Input() name = ''
+
+  protected onChange: (value: unknown) => void = () => {} // eslint-disable-line @typescript-eslint/no-empty-function
+
+  private onTouched: () => void = () => {} // eslint-disable-line @typescript-eslint/no-empty-function
+
+  writeValue(value: unknown) {
+    this.value = value
+  }
+
+  registerOnChange(fn: (value: unknown) => void) {
+    this.onChange = fn
+  }
+
+  registerOnTouched(fn: () => void) {
+    this.onTouched = fn
+  }
+
+  setDisabledState(isDisabled: boolean) {
+    this.isDisabled = isDisabled
+  }
+
+  handleOnBlur() {
+    this.onTouched()
+  }
 }
